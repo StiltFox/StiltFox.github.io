f8e979b3f7949612177f426de8a6ef5099655a64
Cryos <cryos@stiltfox.com>
2023-03-22 21:52:45 -0400

    making index lower case for github pages.

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 var menu = 
 `<div class="menu"> 
 <div class="menu-button-container">
-    <a class="stiltfox-button" href="/Index.html">Home</a>
+    <a class="stiltfox-button" href="/index.html">Home</a>
     <a class="stiltfox-button" href="/pages/MissionStatement.html">About</a>
     <a class="stiltfox-button" href="/pages/Products.html">Products</a>
     <div style="margin-left:auto;"></div>
